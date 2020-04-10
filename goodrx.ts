9843c88e644a9a083896b2caef391e29bb2ae511
Rafael Sousa <rafmsou@gmail.com>
2020-04-10 19:46:01 -0300

    improve goodrx code

--- goodrx.ts
+++ goodrx.ts
@@ -14,36 +14,53 @@ import AdblockerPlugin from "puppeteer-extra-plugin-adblocker"
 puppeteer.use(AdblockerPlugin({ blockTrackers: true }))
 puppeteer.use(StealthPlugin())
 
-const url = 'https://www.goodrx.com/metformin'
+const url = "https://www.goodrx.com/"
+const source = "goodrx"
 
-exports.invoke = (_: express.Request, res: express.Response) => {
+exports.invoke = (req: express.Request, res: express.Response) => {
+
+  if (!req.query.drug) {
+    res.status(200).send({ offers: [], error: "`drug` query param is missing" })
+    return
+  }
+  if (!req.query.zipcode) {
+    res.status(200).send({ offers: [], error: "`zipcode` query param is missing" })
+    return
+  }
 
   puppeteer.launch({ headless: true, args: ["--no-sandbox"] })
   .then(async (browser: Browser) => {
     const page = await browser.newPage()
     const pendingXHR = new PendingXHR(page)
+    const drug = req.query.drug
 
     await page.goto(url, { timeout: 0 })
 
     try {
-      await page.waitForSelector('button[data-qa="set_location_button"]', { timeout: 1000 })
-      await page.click('button[data-qa="set_location_button"]')
-      await page.waitForSelector('#locationModalInput')
-      await page.type('#locationModalInput', '91325')
-      await page.click('#uat-location-submit')
+      await page.waitForSelector("input[data-qa='search_inp']", { timeout: 1000 })
+      await page.type("input[data-qa='search_inp']", drug, { delay: 100 })
+      await page.waitForSelector("nav[data-qa='typeahead'] > a")
+      await page.waitFor(500)
+      await page.click("nav[data-qa='typeahead'] > a")
 
-      await page.waitForSelector('button[data-qa="location_element_after_setting_location"]')
+      await page.waitForSelector("button[data-qa='set_location_button']")
+      await page.click("button[data-qa='set_location_button']")
+      await page.waitForSelector("#locationModalInput")
+      await page.type("#locationModalInput", req.query.zipcode)
+      await page.click("#uat-location-submit")
+
+      await page.waitForSelector("button[data-qa='location_element_after_setting_location']")
       await pendingXHR.waitForAllXhrFinished()
       await page.waitForFunction(
-        () => !document.querySelector('span[class^="spinner-"]'),
-        { polling: 'mutation'},
+        () => !document.querySelector("span[class^='spinner-']"),
+        { polling: "mutation" },
       );
 
       const offers = await page.$$eval(
-        'li[data-qa="price_row"]',
+        "li[data-qa='price_row']",
         (divs: Element[]) => divs.map(div => {
-          const pharmacy = div.querySelector('div[data-qa="store_name"] > span:nth-of-type(2)')
-          const priceElement = div.querySelector('div[data-qa="drug_price"]')
+          const pharmacy = div.querySelector("div[data-qa='store_name'] > span:nth-of-type(2)")
+          const priceElement = div.querySelector("div[data-qa='drug_price']")
           const priceRegexMatch = priceElement
             ? priceElement.textContent?.match(/\$(\d{1,3}(\,\d{3})*|(\d+))(\.\d{2})?/)
             : null
@@ -56,15 +73,15 @@ exports.invoke = (_: express.Request, res: express.Response) => {
       )
 
       const location = await page.$eval(
-        'button[data-qa="location_element_after_setting_location"]',
+        "button[data-qa='location_element_after_setting_location']",
         button => button.textContent
       )
 
-      res.status(200).send({ offers, location })
+      res.status(200).send({ source, offers, location })
       await browser.close()
     } catch (error) {
       console.error(error)
-      res.status(200).send("error on fn")
+      res.status(200).send({ source, offers: [], error })
       await browser.close()
     }
   })
